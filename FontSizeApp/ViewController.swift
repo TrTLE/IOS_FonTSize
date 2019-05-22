4ed295085cc42d9f53864e4c83b88f360530acb7
Alexis P <chpaqou@gmail.com>
2019-05-22 14:19:33 +0200

    Commit 19-22-05 14:19 Creation du projet et ouverture d'une nouvelle page

--- FontSizeApp/ViewController.swift
+++ FontSizeApp/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var ui_FontLabel: UILabel!
+    
+    @IBOutlet weak var ui_ExempleTextFont: UILabel!
+    
+    @IBOutlet weak var ui_SizeFontSteeper: UIStepper!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.  
     }
 
 
+    @IBAction func ChangedFontSteeper(_ sender: Any) {
+        
+        let newsize:Double = ui_SizeFontSteeper.value
+        
+        ui_ExempleTextFont.font = UIFont.systemFont(ofSize:CGFloat(newsize))
+        
+        ui_FontLabel.text = "\(newsize)"
+        
+    }
 }
 
